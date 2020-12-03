8d4bf4873e695993be5020bdb4efdf836e6fe974
Raul Perez <ralberto73@gmail.com>
2020-12-03 09:54:42 -0500

    Web App

---
+++ MyBusiness.Models/ServiceOrder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBusiness.Models
+{
+    public class ServiceOrder
+    {
+        public int  ServiceOrderId{ get; set; }
+        public int ServiceOrderStatusId { get; set; }
+        public string Contact { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public int BrandId { get; set; }
+        public string Model { get; set; }
+        public string SubModel { get; set; }
+        public int Year        { get; set; }
+        public int ProductId { get; set; }
+        public string PaymentMethod { get; set; }
+        public int SupplierId { get; set; }
+        public decimal BillableAmount { get; set; }
+        public decimal LaborAmount { get; set; }
+        public decimal PartCost { get; set; }
+        public string CreatedBy { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+
+    }
+}
+/*
+
+ */
---
+++ MyBusiness.WebApp/Controllers/ServiceOrdersController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBusiness.WebApp.Controllers
+{
+    public class ServiceOrdersController : Controller
+    {
+        // GET: ServiceOrdersController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ServiceOrdersController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ServiceOrdersController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ServiceOrdersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ServiceOrdersController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ServiceOrdersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ServiceOrdersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ServiceOrdersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
