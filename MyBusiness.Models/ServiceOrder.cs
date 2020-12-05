83287e0127b6b2a33b930cfb166451d214126c78
Raul Perez <ralberto73@gmail.com>
2020-12-04 21:48:17 -0500

    Wizard First Aproach

--- MyBusiness.Models/ServiceOrder.cs
+++ MyBusiness.Models/ServiceOrder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,20 +9,35 @@ namespace MyBusiness.Models
 {
     public class ServiceOrder
     {
+        [Key]
         public int  ServiceOrderId{ get; set; }
         public int ServiceOrderStatusId { get; set; }
+
+        [Display(Name = "Contact")]
         public string Contact { get; set; }
         public string Email { get; set; }
+        [Required]
+        [Display(Name = "Phone")]
         public string PhoneNumber { get; set; }
+
+        [Display(Name = "Address Line 1")]
         public string AddressLine1 { get; set; }
+
+        [Display(Name = "Address Line 2")]
         public string AddressLine2 { get; set; }
         public string City { get; set; }
         public string State { get; set; }
-        public string ZipCode { get; set; }
+
+        [Display(Name = "Zip Code")]
+        public string ZipCode { get; set; } 
+
+        [Display(Name = "Brand")]
         public int BrandId { get; set; }
         public string Model { get; set; }
         public string SubModel { get; set; }
         public int Year        { get; set; }
+
+        [Display(Name = "Part")]
         public int ProductId { get; set; }
         public string PaymentMethod { get; set; }
         public int SupplierId { get; set; }
