68ca3a934b7359da9b99c50d49c0a43e56d4feea
Raul Perez <ralberto73@gmail.com>
2020-12-05 21:34:52 -0500

    ServiceOrderStatus

---
+++ MyBusiness.DataAccess/ServiceOrderStatusRepository.cs
@@ -0,0 +1,63 @@
+﻿using MyBusiness.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBusiness.DataAccess
+{
+        public class ServiceOrderStatusRepository
+        {
+            private string _connection_string;
+            private AdoSqlHelper db = null;
+            public ServiceOrderStatusRepository(string connection_string)
+            {
+                this._connection_string = connection_string;
+                db = new AdoSqlHelper(connection_string);
+            }
+
+            public List<ServiceOrderStatus> GetAll() => db.GetListFromSp<ServiceOrderStatus>("ServiceOrderStatus_GetAll", null, null);
+/*
+            public Brand GetById(int id)
+            {
+                return db.GetListFromSp<Brand>("Brands_GetById"
+                                                  , new List<SqlParameter>() { new SqlParameter("@BarandID", System.Data.SqlDbType.Int) }
+                                                  , id).FirstOrDefault<Brand>();
+            }
+
+            public bool Update(Brand brand, string user)
+            {
+                object result = db.GetValueFromSp<int>("Brands_Update"
+                                                , new List<SqlParameter>() {  new SqlParameter("@BarandID",  System.Data.SqlDbType.Int),
+                                                                          new SqlParameter("@BrandName", System.Data.SqlDbType.VarChar,100),
+                                                                          new SqlParameter("@user",      System.Data.SqlDbType.VarChar,100) }
+                                                , brand.BrandId
+                                                , brand.BrandName
+                                                , user);
+                return true; // (result > 0);
+            }
+
+            public int AddNew(Brand brand, string user)
+            {
+                Object result = db.GetValueFromSp<int>("Brands_AddNew"
+                                    , new List<SqlParameter>() {   new SqlParameter("@BrandName", System.Data.SqlDbType.VarChar,100),
+                                                               new SqlParameter("@user",      System.Data.SqlDbType.VarChar,100) }
+                                    , brand.BrandName
+                                    , user);
+                return Convert.ToInt32(result);
+            }
+
+            public int Delete(Brand brand)
+            {
+                Object result = db.GetValueFromSp<int>("Brands_Delete"
+                                    , new List<SqlParameter>() { new SqlParameter("@BarandID", System.Data.SqlDbType.Int) }
+                                    , brand.BrandId);
+                return Convert.ToInt32(result);
+            }
+*/
+
+        }
+    
+}
---
+++ MyBusiness.Models/ServiceOrderStatus.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MyBusiness.Models
+{
+    public class ServiceOrderStatus
+    {
+        [Key]
+     
+        public int ServiceOrderStatusId { get; set; }
+        [Required]
+        [Display(Name = "Brand")]
+        public string StatusName { get; set; }
+
+        [Display(Name = "Color")]
+        public string Color { get; set; }
+
+        [Display(Name = "Icon")]
+        public string IconPicture { get; set; }
+
+        [Display(Name = "Created By")]
+        public string CreatedBy { get; set; }
+
+        [Display(Name = "Updated By")]
+        public string UpdatedBy { get; set; }
+
+        [Display(Name = "Creation date")]
+        public DateTime CreationDate { get; set; }
+
+        [Display(Name = "Last Update")]
+        public DateTime LastUpdateDate { get; set; }
+    }
+}
