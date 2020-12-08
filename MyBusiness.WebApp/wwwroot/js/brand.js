69cb64c1e0ae04725f58cd98d0db3541776e3b23
Raul Perez <ralberto73@gmail.com>
2020-12-08 12:02:03 -0500

    More design

--- MyBusiness.WebApp/wwwroot/js/brand.js
+++ MyBusiness.WebApp/wwwroot/js/brand.js
@@ -12,12 +12,26 @@ function loadDataTable() {
             "datatype" :"json"
         },
         "columns": [
-            { "data": "brandId", "width": "10%" },
             { "data": "brandName", "width": "15%" },
             { "data": "createdBy", "width": "15%" },
             { "data": "creationDate", "width": "15%" },
             { "data": "updatedBy", "width": "15%" },
             { "data": "lastUpdateDate", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                                <a href="/Admin/category/Upsert/${data}" class='btn btn-success text-white' style='cursor:pointer; width:100px;'>
+                                    <i class='far fa-edit'></i> Edit
+                                </a>
+                                &nbsp;
+                                <a onclick=Delete("/Admin/category/Delete/${data}") class='btn btn-danger text-white' style='cursor:pointer; width:100px;'>
+                                    <i class='far fa-trash-alt'></i> Delete
+                                </a>
+                            </div>
+                            `;
+                }, "width": "25%"
+            }
 
         ],
         "language": {
