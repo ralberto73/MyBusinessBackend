2022e40302a947d83c6636a90abe612a6c07583c
Raul Perez <ralberto73@gmail.com>
2020-12-11 08:03:39 -0500

    Service Order Status  Completed

--- MyBusiness.WebApp/wwwroot/js/serviceOrderStatus-list.js
+++ MyBusiness.WebApp/wwwroot/js/serviceOrderStatus-list.js
@@ -28,25 +28,53 @@ function loadDataTable() {
             { "data": "updatedBy", "width": "15%" },
             { "data": "lastUpdateDate", "width": "15%" },
             {
-                "data": " serviceOrderStatusId",
-                "render": function (data) {
+                "data": "serviceOrderStatusId"
+                , "render": function (data) {
                     return `<div class="text-center">
-                                <a href="/Admin/category/Upsert/${data}" class='btn btn-success text-white' style='cursor:pointer; width:100px;'>
+                                <a href="/ServiceOrderStatus/Upsert/${data}" class='btn btn-success text-white' style='cursor:pointer; width:100px;'>
                                     <i class='far fa-edit'></i> Edit
                                 </a>
                                 &nbsp;
-                                <a onclick=Delete("/Admin/category/Delete/${data}") class='btn btn-danger text-white' style='cursor:pointer; width:100px;'>
+                                <a onclick=Delete("/ServiceOrderStatus/Delete/${data}") class='btn btn-danger text-white' style='cursor:pointer; width:100px;'>
                                     <i class='far fa-trash-alt'></i> Delete
                                 </a>
-                            </div>
-                            `;
-                }, "width": "25%"
-            }
+                            </div>`;
+                            }
+                , "width": "25%"
+            },
+
 
         ],
         "language": {
             "emptyTable":"NO records found"
         },
         "width":"100%"
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure you want to delete?",
+        text: "You will not be able to restore the content!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Yes, delete it!",
+        closeOnconfirm: true
+    }, function () {
+        $.ajax({
+            type: 'DELETE',
+            url: url,
+            success: function (data) {
+                if (data.success) {
+                    toastr.success(data.message);
+                    $('#tblData').DataTable().ajax.reload();
+                    // loadDataTable();
+                }
+                else {
+                    toastr.error(data.message);
+                }
+            }
+        });
     });
 }
